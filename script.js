ea1fe227dc9dc75776bbac3364bbd94d7a3f2b6b
juju1710 <161592876+juju1710@users.noreply.github.com>
2024-06-04 10:16:56 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
+const caixaPrincipal=document.querySelector(".caixa-principal);
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado);
 
+const perguntas=[
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "Alternativa 1"
+      "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "Alternativa 1"
+      "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "Alternativa 1"
+      "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "Alternativa 1"
+      "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "Alternativa 1"
+      "alternativa 2"
+     ]
+  },
+];
+
+let atual=0;
+let perguntasAtual;
+
+function mostraPergunta (){
+  perguntaAtual=perguntas [atual];
+  caixaPerguntas.textContent=perguntaAtual.enunciado;
+  mostraAltenativas ();
+}
+
+function mostraAltenativas(){
+   for(const alternativa of perguntasAtual.alternativa){
+   const botaoAlternativas = document.createElement("button");
+   botaoAlternativas.textContent= alternativa;
+   caixaAlternativas.appendChild(botaoAlternativas);
+   }
+}
+
+
+
+
+
+mostraPergunta();
